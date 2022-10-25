9547f744ee869eb62798b20a97f32b6d8634abbe
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-10-25 09:59:41 -0500

    Add Skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+/**
+ * Returns an unordered list of skills
+ * @param {Object} skills - an array of skills
+ * @returns A React component
+ */
+
+const Skills = ({skills}) => {
+  return (
+    <ul>{skills.map((skill, index) => <li key={`skill-${i}`} className="mb-4">{skill}</li>)}</ul>
+  )
+}
+
+export default Skills
