5abc2da6a1750b385537a41ad7c8d8ec444d548a
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-10-25 11:16:32 -0500

    Update Header.jsx, fullName is a method not a property

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@
 const Header = ({contactInfo}) => {
   return (
     <header className="mb-8">
-      <h1 className="font-serif font-bold text-5xl mb-4">{contactInfo.fullName}</h1>
+      <h1 className="font-serif font-bold text-5xl mb-4">{contactInfo.fullName()}</h1>
       <h6 className="font-sans font-medium text-xs">{contactInfo.jobTitle}</h6>
     </header>
   )
