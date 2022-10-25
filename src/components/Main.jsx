9df8febef6f1ecf8b37d0b5a8b435a4f723e685d
Chad Hjelle <Mthrboard@users.noreply.github.com>
2022-10-25 10:02:50 -0500

    Update Main.jsx, move header

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,9 @@ const Main = ({resumeInfo}) => {
   return (
     <main className="basis-128 ml-8 mr-12" aria-label="Main">
       <Header contactInfo={resumeInfo.contactInfo} />
+      <h4 className="sectionHeader">Summary</h4>
       <Summary bio={resumeInfo.contactInfo.bio} />
+      <h4 className="sectionHeader">Work Experience</h4>
       <Experience jobs={resumeInfo.jobs} />
     </main>
   )
